773138e93fa2ff7037037714ce4f8224258644f7
Souvik Alam <polol5842@gmail.com>
2024-02-03 22:00:02 -0500

    Set cooldowns for commands and reload for testing

---
+++ commands/utility/reload.js
@@ -0,0 +1,34 @@
+const { SlashCommandBuilder } = require('discord.js');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('reload')
+		.setDescription('Reloads a command.')
+		.addStringOption(option =>
+			option.setName('command')
+				.setDescription('The command to reload.')
+				.setRequired(true)),
+	async execute(interaction) {
+		// Checking if the command exists to reload
+		const commandName = interaction.options.getString('command', true).toLowerCase();
+		const command = interaction.client.commands.get(commandName);
+
+		if (!command) {
+			return interaction.reply(`There is no command with name \`${commandName}\`!`);
+		}
+
+		// Deleting the previous commands from the client and require the file again
+        delete require.cache[require.resolve(`./${command.data.name}.js`)];
+		
+		// Loading the command file and adding it to the client.commands
+        try {
+            interaction.client.commands.delete(command.data.name);
+            const newCommand = require(`./${command.data.name}.js`);
+            interaction.client.commands.set(newCommand.data.name, newCommand);
+            await interaction.reply(`Command \`${newCommand.data.name}\` was reloaded!`);
+        } catch (error) {
+            console.error(error);
+            await interaction.reply(`There was an error while reloading a command \`${command.data.name}\`:\n\`${error.message}\``);
+        }
+	},
+};
